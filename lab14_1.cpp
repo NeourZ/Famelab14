e84932014dbfec82703e7f00ebf23f0df13f47cf
Fame <pattanitfame@gmail.com>
2024-01-06 11:29:47 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	for (int i = 1 ; i <= N - 1 ; i ++){
+		cout << "Pass " << i << ":" ;
 
+		T temp = d[i];
+		int x;
+		for (x = i-1 ; x >= 0 ; x--){
+			if(d[x] >= temp) break;
+			d[x+1] = d[x];
+		}
+		d[x+1] = temp;
+
+	for(int i = 0; i < 10; i++) cout << d[i] << " ";
+	cout << '\n' ;
+	}
 }
 
 int main(){
